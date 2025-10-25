26232592193666b318530c20e72c458f69623c8a
ATHUL-MURALI <anithaathul916@gmail.com>
2025-10-25 23:42:22 +0530

    stuff

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,12 +1,14 @@
 import './AboutMe.css'
 
 function AboutMe() {
-  return (
-    <div className='container'>
-        <h2>Athul</h2>
-        <p>hi there am athul a inspering software developer here studing how to do stuff</p>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div className='content-box'>
+                <h2>Athul</h2>
+                <p>hi there am athul a inspering software developer here studing how to do stuff</p>
+            </div>
+        </div>
+    )
 }
 
 export default AboutMe
