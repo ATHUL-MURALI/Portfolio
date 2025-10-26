4ba64dc9dc191060eb2bbb69f1f86e43f3056ffd
ATHUL-MURALI <anithaathul916@gmail.com>
2025-10-26 15:46:20 +0530

    stuff

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,11 +1,16 @@
 import './AboutMe.css'
-
+import myImg from '../../assets/athulPic.png'
 function AboutMe() {
     return (
         <div className='container'>
-            <div className='content-box'>
-                <h2>Athul</h2>
-                <p>hi there am athul a inspering software developer here studing how to do stuff</p>
+            <div className='container-box'>
+                <div className='image-box'>
+                    <img src = {myImg} alt="img not found" />
+                </div>
+                <div className='content-box'>
+                    <h2>Athul</h2>
+                    <p>I’m a Full-Stack Developer skilled in React, Spring Boot, JavaScript, Python, ETL tools.</p>
+                </div>
             </div>
         </div>
     )
