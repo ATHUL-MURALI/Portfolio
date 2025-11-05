cb769ba48edd41ccf222c21d4a76e6c1cfb1f53a
ATHUL-MURALI <anithaathul916@gmail.com>
2025-11-06 00:45:04 +0530

    Stuffs

--- src/components/port/Port.jsx
+++ src/components/port/Port.jsx
@@ -1,4 +1,5 @@
 import './Port.css'
+import pf from '../../assets/portf/leetcode.svg' 
 const Port = () => {
     return (
         <div className='Port__container'>
@@ -7,10 +8,22 @@ const Port = () => {
                     <p>Languages & Frameworks</p>
                 </div>
                 <div className='Port__content'>
-                    <h1 className='Port__boxbox'>Linkdin</h1>
-                    <h1 className='Port__boxbox'>Github</h1>
-                    <h1 className='Port__boxbox'>Stack Overflow</h1>
-                    <h1 className='Port__boxbox'>LeetCode</h1>
+                    <div className='Port__boxbox'>
+                        <img src = {pf} alt="error" />
+                        <h1>Linkdin</h1>
+                    </div>
+                    <div className='Port__boxbox'>
+                        <img src = {pf} alt="error" />
+                        <h1>Github</h1>
+                    </div>
+                    <div className='Port__boxbox'>
+                        <img src = {pf} alt="error" />
+                        <h1>Stack Overflow</h1>
+                    </div>
+                    <div className='Port__boxbox'>
+                        <img src = {pf} alt="error" />
+                        <h1>LeetCode</h1>
+                    </div>
                 </div>
             </div>
         </div>
